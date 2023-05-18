84513f9c3811bef67b10339b95852d3dbce20ac8
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2023-05-17 23:07:54 -0400

    Final seccion 4

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ListComponent } from "./list/list.component";
+import { HeroComponent } from "./hero/hero.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations:[
+    ListComponent,
+    HeroComponent
+  ],
+  imports:[CommonModule],
+  exports:[HeroComponent, ListComponent]
+})
+export class HeroesModule {}
